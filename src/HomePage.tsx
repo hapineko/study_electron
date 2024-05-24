c4ee29ee87c9973381fbed6ec42e2abe4f485c5f
hapineko <pr13h31m@gmail.com>
2024-05-24 15:45:30 +0900

    wip: darkmode

---
+++ src/HomePage.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+interface Window {
+  darkMode: any;
+}
+declare var window: Window;
+
+export default function HomePage() {
+  const [mode, setMode] = useState("");
+
+  async function toggleDarkMode() {
+    const isDarkMode = await window.darkMode.toggle();
+    const isMode = isDarkMode ? "Dark" : "Light";
+    setMode(isMode);
+  }
+
+  async function resetToSystem() {
+    await window.darkMode.system()
+    setMode('System');
+  }
+
+  return (
+    <div>
+      <h1 className="bg-slate-700">Hello World!</h1>
+      <p>
+        Current theme source: <strong id="theme-source">{mode}</strong>
+      </p>
+
+      <button id="toggle-dark-mode" onClick={toggleDarkMode}>
+        Toggle Dark Mode
+      </button>
+      <button id="reset-to-system" onClick={resetToSystem}>Reset to System Theme</button>
+    </div>
+  );
+}
--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,10 @@
 import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+import HomePage from './HomePage';
+
+const domNode = document.getElementById('app');
+const root = createRoot(domNode);
+
+root.render(
+  <HomePage />
+);
